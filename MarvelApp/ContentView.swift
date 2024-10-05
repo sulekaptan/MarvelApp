51c5a97e39c896c196ac5db66e687684f3edfbc1
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-10-05 23:10:18 +0300

    Initial commit

---
+++ MarvelApp/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  MarvelApp
+//
+//  Created by Şule Kaptan on 4.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var backgroundColor: Color = Color(UIColor(named: "bgColor")!)
+    
+    @State private var isAnimating = false
+    @State private var isActive = false
+    
+    var body: some View {
+        
+        if isActive{
+            LoginView()
+        }
+        else {
+            ZStack {
+                backgroundColor
+                    .edgesIgnoringSafeArea(.all)
+            
+                VStack {
+                    Spacer()
+                    
+                    Image("marvel")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 200)
+                            .scaleEffect(isAnimating ? 1.5 : 1.0)
+                            .onAppear() {
+                                withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
+                                    self.isAnimating = true
+                                }
+                            }
+                    
+                    Spacer()
+                    
+                    Text("Powered by Şule Kaptan")
+                        .font(.footnote)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Şule Kaptan on 4.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
