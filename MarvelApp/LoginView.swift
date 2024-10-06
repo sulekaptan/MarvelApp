bd2edba8e31f4ae293f169e4c7c6484e6917bfcd
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-10-06 17:04:05 +0300

    login screen

--- MarvelApp/LoginView.swift
+++ MarvelApp/LoginView.swift
@@ -8,8 +8,90 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var backgroundColor: Color = Color(UIColor(named: "bgColor")!)
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+                VStack (spacing: 30) {
+                    Image("marvel")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .padding(.top, 80)
+                
+                    
+                    VStack(spacing: 15) {
+                        TextField("E-mail", text: $email)
+                            .padding()
+                            .background(Color.white.opacity(1))
+                            .cornerRadius(50)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        
+                        SecureField("Password", text: $password)
+                            .padding()
+                            .background(Color.white.opacity(1))
+                            .cornerRadius(50)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                 
+                    VStack(spacing: 15) {
+                        Button(action: {
+                            //Login
+                        }) {
+                            Text("Login")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.black)
+                                .foregroundColor(.white)
+                                .cornerRadius(50)
+                        }
+                        .padding(.horizontal, 50)
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text("Hata"), message: Text(alertMessage), dismissButton: .default(Text("Tamam")))
+                        }
+                            
+                        Button(action: {
+                            //Login
+                        }) {
+                            Text("Login with Google")
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.black)
+                                .foregroundColor(.white)
+                                .cornerRadius(50)
+                        }
+                        .padding(.horizontal, 50)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            // Kayıt ekranına yönlendirme
+                        }) {
+                            Text("Don't have an account? Sign up")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                                .underline() // Altı çizili
+                        }
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 20)
+                    }
+                }
+        }
     }
 }
 
